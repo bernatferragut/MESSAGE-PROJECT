b9a199972fc01b20521fff715f4a29d43d5ba501
bernat <elbernat@gmail.com>
2019-05-09 11:12:38 -0400

    Matterjs-start

---
+++ 4.Physics/main.js
@@ -0,0 +1,14 @@
+
+let width = window.innerWidth;
+let height = window.innerHeight;
+
+
+function setup(){
+    
+    createCanvas(width, height)
+    background('black')
+}
+
+function draw() {
+    // code
+}
